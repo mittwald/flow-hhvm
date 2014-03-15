1e24e80c60a61fdcdfa905fe6212359686a1047c
Martin Helmich <kontakt@martin-helmich.de>
2014-03-15 17:46:04 +0100

    [TASK] Add license and source code documentation.

--- Classes/Mittwald/HHVM/Composer/Installer.php
+++ Classes/Mittwald/HHVM/Composer/Installer.php
@@ -1,19 +1,66 @@
 <?php
+/*
+ * Copyright (c) 2014 Martin Helmich <m.helmich@mittwald.de>
+ *                    Mittwald CM Service GmbH & Co.KG
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
 namespace Mittwald\HHVM\Composer;
 
 use Composer\IO\IOInterface;
 use Composer\Script\CommandEvent;
 use Symfony\Component\Yaml\Yaml;
 
+
+/**
+ * Installer script for making TYPO3 Flow and Neos installations compatible with HHVM.
+ *
+ * @author     Martin Helmich <m.helmich@mittwald.de>
+ * @package    Mittwald\HHVM
+ * @subpackage Composer
+ */
 class Installer {
 
+	/**
+	 * Path that this package is stored under.
+	 * TODO: Can this be determined automatically?
+	 */
 	const PACKAGE_PATH = 'Packages/Application/Mittwald.HHVM';
 
+	/**
+	 * Event that should be called whenever "composer install" or "composer update" is called.
+	 *
+	 * @param CommandEvent $event The composer event.
+	 * @return void
+	 */
 	static public function postInstall(CommandEvent $event) {
 		$io = $event->getIO();
 
-		$patches = ['flow-2.2.x.patch'];
+		$patches = [];
 
+		// TODO: Is there any better way to determine which packages are installed?
+		//       Maybe any composer APIs that can be accessed from within this event!?
+		// TODO: Can we determine the version numbers automatically!?
+		if (is_dir('Packages/Framework/TYPO3.Flow')) {
+			$patches[] = 'flow-2.2.x.patch';
+		}
 		if (is_dir('Packages/Applications/TYPO3.Neos')) {
 			$patches[] = 'neos-1.0.x.patch';
 		}
@@ -36,6 +83,21 @@ class Installer {
 		]);
 	}
 
+	/**
+	 * Adds some special entries to the Settings.yaml.
+	 *
+	 * This method checks if a Settings.yaml is already present (if not, one will be
+	 * created) and adds the following configuration options:
+	 *
+	 *     TYPO3:
+	 *       Flow:
+	 *         core:
+	 *           phpBinaryPathAndFilename: /usr/bin/hhvm  # Is determined automatically
+	 *           subRequestPhpIniPathAndFilename: false   # HHVM does not have a php.ini
+	 *
+	 * @param IOInterface $io Composer's IO interface.
+	 * @throws \Exception
+	 */
 	static private function fixConfigurationFile(IOInterface $io) {
 		$configurationFile = 'Configuration/Settings.yaml';
 
@@ -83,19 +145,13 @@ class Installer {
 		$io->write('  - Wrote <comment>' . $configurationFile . '</comment>.');
 	}
 
-	static private function copyHhvmConfigFile(IOInterface $io) {
-		$templatePath = sprintf('%s/Configuration/HipHopJit.hdf', self::PACKAGE_PATH);
-		if (!file_exists($templatePath)) {
-			throw new \Exception('The file "' . $templatePath . '" does not exist!');
-		}
-
-		$contents = file_get_contents($templatePath);
-		$contents = str_replace('###PATH###', getcwd(), $contents);
-
-		file_put_contents('Configuration/HipHopJit.hdf', $contents);
-		$io->write('<info>Created <comment>Configuration/HipHopJit.hdf</comment>.</info>');
-	}
-
+	/**
+	 * Applies some patches to various packages to account for HHVM bugs.
+	 *
+	 * @param IOInterface $io Composer's IO interface.
+	 * @param array $patches A list of patch files that should be applied.
+	 * @throws \Exception
+	 */
 	static private function applyPatches(IOInterface $io, array $patches) {
 		$patchBinary = exec('which patch', $output, $result);
 		if ($result !== 0 || !$patchBinary) {
@@ -130,7 +186,7 @@ class Installer {
 					$lines = explode("\n", $response);
 					$lcount = count($lines);
 
-					for ($i = 0; $i < $lcount; $i ++) {
+					for ($i = 0; $i < $lcount; $i++) {
 						if (strpos(strtolower($lines[$i]), 'reversed') !== FALSE) {
 							$io->write('    <info>' . $lines[$i] . '</info>');
 							$io->write('    <info>' . $lines[++$i] . '</info>');
@@ -149,6 +205,28 @@ class Installer {
 				throw new \Exception('Could not execute "' . $command . '".');
 			}
 		}
+	}
+
+	/**
+	 * Copies a HDF file to the global configuration directory.
+	 *
+	 * Copies the HDF file containing some default configuration for HHVM to the global
+	 * Configuration directory and replaces some markers within this file.
+	 *
+	 * @param IOInterface $io Composer's IO interface.
+	 * @throws \Exception
+	 */
+	static private function copyHhvmConfigFile(IOInterface $io) {
+		$templatePath = sprintf('%s/Configuration/HipHopJit.hdf', self::PACKAGE_PATH);
+		if (!file_exists($templatePath)) {
+			throw new \Exception('The file "' . $templatePath . '" does not exist!');
+		}
+
+		$contents = file_get_contents($templatePath);
+		$contents = str_replace('###PATH###', getcwd(), $contents);
+
+		file_put_contents('Configuration/HipHopJit.hdf', $contents);
+		$io->write('<info>Created <comment>Configuration/HipHopJit.hdf</comment>.</info>');
 	}
 
 }
